38cd856f02052fee9d5cb77ad523d269d3e3978b
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-01-19 19:03:33 +0530

    Backend AUTH APIs Completed. Signup, Login, Logout, Checking Auth

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import {dbConnection} from './database/dbConnection.js';
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    res.send('Server is ready, test');
-})
+// app.use(express.json());  //allows us to parse incoming request (json data) in req.body (body of the request), data will be in json format
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000')
+
+// Middleware
+app.use(express.json()); //allows us to parse incoming request (json data) in req.body (body of the request), data will be in json format
+app.use(cookieParser()); //allows us to parse incoming request (cookie data) in req.cookies, data will be in json format
+
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
 });
 
- dbConnection();
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
